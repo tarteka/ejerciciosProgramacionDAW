1954a4708e3cda0f0ee5baf59afee50649c95fa0
tarteka <info@tarteka.net>
2021-07-13 20:07:15 +0200

    añadir comentarios

--- UD1/HolaMundo.java
+++ UD1/HolaMundo.java
@@ -1,8 +1,18 @@
+/* 
+   Nuestro primer programa: HolaMundo
+   Visualiza por consola el mensaje "Hola mundo"
+   Nos ha permitido estudiar los elementos básicos de un programa en Java y
+   los aspectos principales para mejorar su legibilidad y comprensión
+*/
+
+// Definimos la clase HolaMundo para crear el programa
+// El nombre del fichero .java debe coincidir exactamente con el nombre de la clase
 public class HolaMundo {
+   // Creamos el método main que indica dónde empieza a ejecutarse el programa
    public static void main(String[] args) {
-      System.out.println("Hola mundo");
+      System.out.println("Hola mundo"); // Mostramos mensajes por consola
       System.out.println();
       System.out.println("¿Qué tal?");
-      System.out.println("Hola \"amigo\"");
+      System.out.println("Hola \"amigo\""); // Carácter especial /" para mostrar las " por consola
    }
 }
